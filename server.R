4da06e70b4251d12a45ed332bf74d83f251fcb10
caten8 <durbinmail@gmail.com>
2016-09-18 02:54:20 -0500

    Added documentation to R files

--- server.R
+++ server.R
@@ -1,3 +1,11 @@
+# Documentation
+# This Shiny app acquires input values from the user to calculate Body Mass Index for a 
+# variety of heights and weights. It contains two inputs, one for height in inches and one 
+# for weight in pounds, and calculates BMI. The function takes two arguments, height and 
+# weight, and returns a numerical value based on a simple formula.
+# This is the back end file.
+
+
 BMI = function(height,weight){
         return(0.45455*weight/(.0254*height)^2)
 }
--- ui.R
+++ ui.R
@@ -1,3 +1,10 @@
+# Documentation
+# This Shiny app acquires input values from the user to calculate Body Mass Index for a 
+# variety of heights and weights. It contains two inputs, one for height in inches and one 
+# for weight in pounds, and calculates BMI. The function takes two arguments, height and 
+# weight, and returns a numerical value based on a simple formula.
+# This is the front end file.
+
 shinyUI(pageWithSidebar(
         headerPanel("BMI Calculator (Body Mass Index)"),
         sidebarPanel(
@@ -15,5 +22,3 @@ shinyUI(pageWithSidebar(
                 h6(em('Reactive output displayed as a result of server calculations.'))
         )
 ))
-
-
